8ad2801b751f376bd9eb9145953221f1158f8184
Bruno0Paz0A1 <99370217+Bruno0Paz0A1@users.noreply.github.com>
2022-03-30 20:08:57 -0400

    Add files via upload

---
+++ pila.py
@@ -0,0 +1,9 @@
+#Ejemplo de pila - LIFO
+Pila=[]
+#incluir elementos - push -apilar
+Pila.append('c')
+Pila.append('a')
+print(Pila)
+#sacar elementos - pop - desapilar
+x=Pila.pop()
+print(Pila)  #lo restante
