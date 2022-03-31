9db7dc654e112c5c727535074b0176bdb92eceb2
Bruno0Paz0A1 <99370217+Bruno0Paz0A1@users.noreply.github.com>
2022-03-30 20:08:42 -0400

    Add files via upload

---
+++ cola.py
@@ -0,0 +1,10 @@
+#Ejemplo de pila - LIFO
+Cola=[]
+#incluir elementos - push -apilar
+Cola.append('c')
+Cola.append('a')
+Cola.append('b')
+print(Cola)
+#sacar elementos - pop - desapilar
+x=Cola.pop()
+print(Cola)  #lo restante
